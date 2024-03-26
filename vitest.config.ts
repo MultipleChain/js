b9639f2bc8a196a62b5fce825e4695a35a3b2427
Halil Beycan <50718965+BeycanDeveloper@users.noreply.github.com>
2024-03-26 18:56:26 +0800

    updated

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,6 +6,7 @@ export default mergeConfig(
     viteConfig,
     defineConfig({
         test: {
+            testTimeout: 180000,
             environment: 'jsdom',
             exclude: [...configDefaults.exclude, 'e2e/*'],
             root: fileURLToPath(new URL('./', import.meta.url)),
