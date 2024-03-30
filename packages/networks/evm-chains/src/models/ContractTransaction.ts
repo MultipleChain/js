e58b86fb57eb6463811ff000c41f148aad56b1d9
Halil Beycan <50718965+BeycanDeveloper@users.noreply.github.com>
2024-03-30 23:05:59 +0800

    done ContractTransaction

--- packages/networks/evm-chains/src/models/ContractTransaction.ts
+++ packages/networks/evm-chains/src/models/ContractTransaction.ts
@@ -1,8 +1,27 @@
 import { Transaction } from './Transaction.ts'
+import { Interface, type TransactionDescription } from 'ethers'
 import type { ContractTransactionInterface } from '@multiplechain/types'
 
 export class ContractTransaction extends Transaction implements ContractTransactionInterface {
-    getAddress(): string {
-        return 'example'
+    /**
+     * @returns {Promise<string>}
+     */
+    async getAddress(): Promise<string> {
+        const data = await this.getData()
+        return data?.response.to ?? ''
+    }
+
+    /**
+     * @param {object[]} abiArray
+     * @returns {Promise<any>}
+     */
+    async decodeData(abiArray: object[]): Promise<TransactionDescription | null> {
+        const data = await this.getData()
+        if (data === null) return null
+        const iface = new Interface(abiArray)
+        return iface.parseTransaction({
+            data: data?.response.data ?? '',
+            value: data?.response.value ?? 0
+        })
     }
 }
--- packages/networks/evm-chains/tests/models.spec.ts
+++ packages/networks/evm-chains/tests/models.spec.ts
@@ -1,14 +1,17 @@
 import { describe, it, expect } from 'vitest'
 
+import ERC20 from '../resources/erc20.json'
 import { Transaction } from '../src/models/Transaction.ts'
 import { TransactionStatusEnum } from '@multiplechain/types'
+import { ContractTransaction } from '../src/models/ContractTransaction.ts'
 
-const txId = '0x566002399664e92f82ed654c181095bdd7ff3d3f1921d963257585891f622251'
+const etherTransferTx = '0x566002399664e92f82ed654c181095bdd7ff3d3f1921d963257585891f622251'
+const tokenTransferTx = '0xdabda3905e585db91768f2ef877f7fbef7c0e8612c0a09c7b379981bdbc48975'
 
 describe('Transaction', () => {
-    const tx = new Transaction(txId)
+    const tx = new Transaction(etherTransferTx)
     it('Id', async () => {
-        expect(tx.getId()).toBe(txId)
+        expect(tx.getId()).toBe(etherTransferTx)
     })
 
     it('Data', async () => {
@@ -44,5 +47,20 @@ describe('Transaction', () => {
 
     it('Status', async () => {
         expect(await tx.getStatus()).toBe(TransactionStatusEnum.CONFIRMED)
+    })
+})
+
+describe('Contract Transaction', () => {
+    const tx = new ContractTransaction(tokenTransferTx)
+
+    it('Address', async () => {
+        expect((await tx.getAddress()).toLowerCase()).toBe(
+            '0x4294cb0dD25dC9140B5127f247cBd47Eeb673431'.toLowerCase()
+        )
+    })
+
+    it('Decode Data', async () => {
+        const result = await tx.decodeData(ERC20)
+        expect(result?.args[0]).toBe('0xbBa4d06D1cEf94b35aDeCfDa893523907fdD36DE')
     })
 })
