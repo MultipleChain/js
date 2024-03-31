333202e93209801e2780c39cf229bb4a522a6c41
Halil Beycan <50718965+BeycanDeveloper@users.noreply.github.com>
2024-03-31 22:17:49 +0800

    updated

--- packages/networks/evm-chains/tests/setup.ts
+++ packages/networks/evm-chains/tests/setup.ts
@@ -1,16 +1,33 @@
 import { Provider } from '../src/services/Provider.ts'
 
-export const provider = new Provider({
-    id: 11155111,
-    hexId: '0xaa36a7',
-    mainnetId: 1,
-    name: 'Ethereum Sepolia Testnet (QR)',
-    rpcUrl: 'https://dimensional-icy-tent.ethereum-sepolia.quiknode.pro/e6a31d965b3ab5fc57d5980d187121f1894c0eb4/',
-    wsUrl: 'wss://dimensional-icy-tent.ethereum-sepolia.quiknode.pro/e6a31d965b3ab5fc57d5980d187121f1894c0eb4/',
-    explorerUrl: 'https://sepolia.etherscan.io/',
-    nativeCurrency: {
-        symbol: 'ETH',
-        name: 'Ethereum',
-        decimals: 18
-    }
-})
+const useCustomRpcAndWs = true
+
+const rpcAndWs = {
+    rpcUrl: 'https://sepolia.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
+    wsUrl: 'wss://sepolia.infura.io/ws/v3/9aa3d95b3bc440fa88ea12eaa4456161'
+}
+
+if (useCustomRpcAndWs) {
+    rpcAndWs.rpcUrl =
+        'https://dimensional-icy-tent.ethereum-sepolia.quiknode.pro/e6a31d965b3ab5fc57d5980d187121f1894c0eb4/'
+    rpcAndWs.wsUrl =
+        'wss://dimensional-icy-tent.ethereum-sepolia.quiknode.pro/e6a31d965b3ab5fc57d5980d187121f1894c0eb4/'
+}
+
+export const provider = new Provider(
+    Object.assign(
+        {
+            id: 11155111,
+            hexId: '0xaa36a7',
+            mainnetId: 1,
+            name: 'Ethereum Sepolia Testnet (QR)',
+            explorerUrl: 'https://sepolia.etherscan.io/',
+            nativeCurrency: {
+                symbol: 'ETH',
+                name: 'Ethereum',
+                decimals: 18
+            }
+        },
+        rpcAndWs
+    )
+)
