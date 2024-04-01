c80a4f5ac12f415810eeaa1c69b5cf4eabe1f0d2
Halil Beycan <50718965+BeycanDeveloper@users.noreply.github.com>
2024-04-01 14:30:42 +0800

    updated for easy use

--- packages/networks/evm-chains/src/index.ts
+++ packages/networks/evm-chains/src/index.ts
@@ -1,32 +1,8 @@
-import * as NFT from './assets/NFT.ts'
-import * as Coin from './assets/Coin.ts'
-import * as Token from './assets/Token.ts'
-import * as Contract from './assets/Contract.ts'
+export * from './services/Provider.ts'
 
-import * as Transaction from './models/Transaction.ts'
-import * as NftTransaction from './models/NftTransaction.ts'
-import * as CoinTransaction from './models/CoinTransaction.ts'
-import * as TokenTransaction from './models/TokenTransaction.ts'
-import * as ContractTransaction from './models/ContractTransaction.ts'
+export * as assets from './assets/index.ts'
+export * as models from './models/index.ts'
+export * as services from './services/index.ts'
 
 export * as utils from '@multiplechain/utils'
 export * as types from '@multiplechain/types'
-
-export * as Provider from './services/Provider.ts'
-export * as TransactionSigner from './services/TransactionSigner.ts'
-export * as TransactionListener from './services/TransactionListener.ts'
-
-export const assets = {
-    NFT,
-    Coin,
-    Token,
-    Contract
-}
-
-export const models = {
-    Transaction,
-    NftTransaction,
-    CoinTransaction,
-    TokenTransaction,
-    ContractTransaction
-}
