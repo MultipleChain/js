c31739818e2e4032dd4411fc1ddc3678bc6c43be
Halil Beycan <50718965+BeycanDeveloper@users.noreply.github.com>
2024-04-06 02:36:18 +0800

    lint and test

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,8 +6,10 @@ export default mergeConfig(
     viteConfig,
     defineConfig({
         test: {
+            watch: false,
             testTimeout: 180000,
-            environment: 'jsdom',
+            environment: 'node',
+            setupFiles: ['./packages/networks/evm-chains/tests/setup.ts'],
             exclude: [...configDefaults.exclude, 'e2e/*'],
             root: fileURLToPath(new URL('./', import.meta.url))
         }
