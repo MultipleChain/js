7efc54c56624b12d09f5461c662fd356f2796d8d
Halil Beycan <50718965+BeycanDeveloper@users.noreply.github.com>
2024-04-20 20:29:56 +0800

    updated

--- packages/networks/boilerplate/src/browser/index.ts
+++ packages/networks/boilerplate/src/browser/index.ts
@@ -1,9 +1,28 @@
 import { Wallet } from './Wallet.ts'
-import * as adapters from './adapters/index.ts'
+import * as adapterList from './adapters/index.ts'
+import type {
+    WalletAdapterListType,
+    WalletAdapterInterface,
+    RegisterWalletAdapterType
+} from '@multiplechain/types'
+
+
+const adapters: WalletAdapterListType = {
+    ...adapterList
+}
+
+const registerAdapter: RegisterWalletAdapterType = (adapter: WalletAdapterInterface): void => {
+    if (Object.values(adapters).find((a) => a.id === adapter.id) !== undefined) {
+        throw new Error(`Adapter with id ${adapter.id} already exists`)
+    }
+
+    adapters[adapter.id] = adapter
+}
 
 export * from '../index.ts'
 
 export const browser = {
     Wallet,
-    adapters
+    adapters,
+    registerAdapter
 }
