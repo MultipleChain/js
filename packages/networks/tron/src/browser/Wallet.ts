6a66c86bf243d163f878d55e1bb468429c1f851d
Halil Beycan <50718965+BeycanDeveloper@users.noreply.github.com>
2024-05-03 12:11:52 +0800

    completed Wallet and added TronLink adapter

--- packages/networks/tron/src/browser/Wallet.ts
+++ packages/networks/tron/src/browser/Wallet.ts
@@ -1,18 +1,63 @@
-import type {
-    WalletInterface,
-    WalletAdapterInterface,
-    WalletPlatformEnum,
-    TransactionSignerInterface
+import {
+    type WalletInterface,
+    type WalletAdapterInterface,
+    type WalletPlatformEnum,
+    type TransactionSignerInterface,
+    ErrorTypeEnum,
+    type ProviderInterface
 } from '@multiplechain/types'
+import { Provider } from '../services/Provider.ts'
+import type { Adapter, AdapterEvents } from '@tronweb3/tronwallet-abstract-adapter'
+
+export interface CustomAdapter extends Adapter {
+    network?: () => Promise<any>
+}
+
+const rejectMap = (error: any, reject: (a: any) => any): any => {
+    console.error('MultipleChain Tron Wallet Error:', error)
+
+    const errorMessage = String(error.message ?? '')
+
+    if (error === 'Cannot transfer TRX to the same account') {
+        return reject(new Error(ErrorTypeEnum.WALLET_CONNECTION_FAILED))
+    }
+
+    if (typeof error === 'object') {
+        if (
+            error.name === 'WalletSignTransactionError' ||
+            errorMessage.includes('Confirmation declined by user') ||
+            errorMessage.includes('User rejected the request.') ||
+            errorMessage.includes('The user rejected connection.') ||
+            errorMessage.includes('Modal is closed.') ||
+            errorMessage.includes('User canceled') ||
+            errorMessage.includes('User rejected')
+        ) {
+            return reject(new Error(ErrorTypeEnum.WALLET_REQUEST_REJECTED))
+        } else if (errorMessage.includes('The wallet is not found.')) {
+            return reject(new Error(ErrorTypeEnum.WALLET_CONNECTION_FAILED))
+        } else if (errorMessage.includes('User disapproved requested chains')) {
+            return reject(new Error(ErrorTypeEnum.UNACCEPTED_CHAIN))
+        } else if (errorMessage.includes('The QR window is closed.')) {
+            return reject(new Error(ErrorTypeEnum.CLOSED_WALLETCONNECT_MODAL))
+        }
+    }
+
+    return reject(error)
+}
 
 export class Wallet implements WalletInterface {
     adapter: WalletAdapterInterface
 
+    walletProvider: CustomAdapter
+
+    networkProvider: Provider
+
     /**
      * @param {WalletAdapterInterface} adapter
      */
-    constructor(adapter: WalletAdapterInterface) {
+    constructor(adapter: WalletAdapterInterface, provider?: Provider) {
         this.adapter = adapter
+        this.networkProvider = provider ?? Provider.instance
     }
 
     /**
@@ -44,9 +89,9 @@ export class Wallet implements WalletInterface {
     }
 
     /**
-     * @returns {string}
+     * @returns {string | undefined}
      */
-    getDownloadLink(): string {
+    getDownloadLink(): string | undefined {
         return this.adapter.downloadLink
     }
 
@@ -55,45 +100,92 @@ export class Wallet implements WalletInterface {
      * @param {object} ops
      * @returns {string}
      */
-    createDeepLink(url: string, ops?: object): string {
+    createDeepLink(url: string, ops?: object): string | null {
+        if (this.adapter.createDeepLink === undefined) {
+            return null
+        }
+
         return this.adapter.createDeepLink(url, ops)
     }
 
     /**
-     * connect to adapter
+     * @param {ProviderInterface} provider
+     * @param {Object} ops
      * @returns {Promise<string>}
      */
-    async connect(): Promise<string> {
-        await this.adapter.connect()
-        return 'wallet address'
+    async connect(provider?: ProviderInterface, ops?: object): Promise<string> {
+        return await new Promise((resolve, reject) => {
+            this.adapter
+                .connect(provider, ops)
+                .then(async (provider) => {
+                    this.walletProvider = provider as CustomAdapter
+
+                    if (
+                        this.walletProvider.network !== undefined &&
+                        this.networkProvider.node.id !==
+                            (await this.walletProvider.network()).chainId
+                    ) {
+                        reject(ErrorTypeEnum.UNACCEPTED_CHAIN)
+                        return
+                    }
+
+                    resolve(await this.getAddress())
+                })
+                .catch((error) => {
+                    const customReject = (error: any): void => {
+                        if (error.message === ErrorTypeEnum.WALLET_REQUEST_REJECTED) {
+                            reject(new Error(ErrorTypeEnum.WALLET_CONNECT_REJECTED))
+                        } else {
+                            reject(error)
+                        }
+                    }
+                    rejectMap(error, customReject)
+                })
+        })
     }
 
     /**
      * @returns {boolean}
      */
-    isDetected(): boolean {
-        return this.adapter.isDetected()
+    async isDetected(): Promise<boolean> {
+        return await this.adapter.isDetected()
     }
 
     /**
      * @returns {boolean}
      */
-    isConnected(): boolean {
-        return this.adapter.isConnected()
+    async isConnected(): Promise<boolean> {
+        return await this.adapter.isConnected()
+    }
+
+    /**
+     * @returns {Promise<string>}
+     */
+    async getChainId(): Promise<string> {
+        return this.networkProvider.node.id
     }
 
     /**
      * @returns {Promise<string>}
      */
     async getAddress(): Promise<string> {
-        return 'wallet address'
+        return (this.walletProvider.address as string).trim()
     }
 
     /**
      * @param {string} message
      */
     async signMessage(message: string): Promise<string> {
-        return 'signed message'
+        return await new Promise((resolve, reject) => {
+            this.walletProvider
+                .signMessage(message)
+                .then((signature: string) => {
+                    resolve(signature)
+                })
+                .catch((error: any) => {
+                    reject(error)
+                })
+        })
     }
 
     /**
@@ -101,7 +193,10 @@ export class Wallet implements WalletInterface {
      * @returns {Promise<string>}
      */
     async sendTransaction(transactionSigner: TransactionSignerInterface): Promise<string> {
-        return 'transaction hash'
+        const signedTx = await this.walletProvider.signTransaction(transactionSigner.getRawData())
+        const { transaction } = await this.networkProvider.tronWeb.trx.sendRawTransaction(signedTx)
+        if (transaction === undefined) throw new Error(ErrorTypeEnum.TRANSACTION_CREATION_FAILED)
+        return transaction.txID as string
     }
 
     /**
@@ -110,6 +205,10 @@ export class Wallet implements WalletInterface {
      * @returns {void}
      */
     on(eventName: string, callback: (...args: any[]) => void): void {
-        'wallet events'
+        if (this.adapter?.provider.on !== undefined) {
+            this.adapter.provider.on(eventName, callback)
+        } else {
+            this.walletProvider.on(eventName as keyof AdapterEvents, callback)
+        }
     }
 }
--- packages/networks/tron/src/browser/adapters/index.ts
+++ packages/networks/tron/src/browser/adapters/index.ts
@@ -1,1 +1,1 @@
-export { default as Example } from './Example.ts'
+export { default as TronLink } from './TronLink.ts'
