b06b9371c05dad12aa7976220ccc09d9f64abc87
Halil Beycan <50718965+BeycanDeveloper@users.noreply.github.com>
2024-05-21 12:49:25 +0800

    listeners excluded

--- vitest.config.ts
+++ vitest.config.ts
@@ -10,17 +10,17 @@ export default mergeConfig(
                 provider: 'istanbul',
                 include: ['**/packages/**/src/**', '**/packages/networks/**/src/**'],
                 exclude: [
-                    '**/boilerplate/**',
                     '**/types/**',
                     '**/browser/**',
                     '**/index.ts/**',
+                    '**/boilerplate/**',
                     '**/bitcoin/src/assets/NFT.ts',
                     '**/bitcoin/src/assets/Token.ts',
                     '**/bitcoin/src/assets/Contract.ts',
                     '**/bitcoin/src/models/NftTransaction.ts',
                     '**/bitcoin/src/models/TokenTransaction.ts',
                     '**/bitcoin/src/models/ContractTransaction.ts',
-                    '**/tron/src/services/TransactionListener.ts'
+                    '**/networks/**/src/services/TransactionListener.ts'
                 ]
             },
             watch: false,
